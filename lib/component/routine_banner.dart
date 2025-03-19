e1b09a388426e35192050854aec405ed3ffecf1f
TechNing39 <leeyujineee@gmail.com>
2025-03-19 21:36:59 +0900

    [Feat] 루틴 배너 구현

---
+++ lib/component/routine_banner.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class RoutineBanner extends StatelessWidget {
+  const RoutineBanner({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = TextStyle(fontFamily: 'RubikSprayPaint', fontSize: 14.0);
+
+    return Container(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('Routine', style: textStyle),
+            IconButton(
+              icon: SvgPicture.asset('android/assets/images/plus.svg'),
+              onPressed: () => print('plus'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
