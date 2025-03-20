7b58415c91624fc80f2d277d14adf395cfdca17e
TechNing39 <leeyujineee@gmail.com>
2025-03-21 01:36:43 +0900

    [Feature] 투두 카드 구현

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:ohmo/component/main_calendar.dart';
 import 'package:ohmo/component/routine_banner.dart';
 import 'package:ohmo/component/routine_card.dart';
 import 'package:ohmo/component/todo_banner.dart';
+import 'package:ohmo/component/todo_card.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -28,15 +29,31 @@ class _HomeScreenState extends State<HomeScreen> {
               selectedDate: selectedDate,
               onDaySelected: onDaySelected,
             ),
-            RoutineBanner(
-
-            ),
-            RoutineCard(content: '오모 회의',
-            ),
-            RoutineCard(content: '데이트',
-            ),
-            TodoBanner(
-
+            Expanded(
+              child: Scrollbar(
+                thickness: 2.0,
+                radius: const Radius.circular(10),
+                child: ListView(
+                  children: [
+                    RoutineBanner(),
+                    RoutineCard(content: '오모 회의'),
+                    RoutineCard(content: '데이트'),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Divider(color: Colors.grey),
+                    ),
+                    TodoBanner(),
+                    TodoCard(content: '코딩'),
+                    TodoCard(content: '운동'),
+                    TodoCard(content: '코딩'),
+                    TodoCard(content: '운동'),
+                    TodoCard(content: '코딩'),
+                    TodoCard(content: '운동'),
+                    TodoCard(content: '코딩'),
+                    TodoCard(content: '운동'),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
