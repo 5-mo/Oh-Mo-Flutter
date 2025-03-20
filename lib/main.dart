3858c955fa94f4432a26fee84a5e8a5117218ba8
TechNing39 <leeyujineee@gmail.com>
2025-03-20 23:25:59 +0900

    [Chore] 배경색 설정

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,9 @@ import 'package:ohmo/screen/home_screen.dart';
 void main() {
   runApp(
     MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+      ),
       home:HomeScreen(),
     ),
   );
