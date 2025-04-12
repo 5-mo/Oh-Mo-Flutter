9864959634d3bf0369f0f7d649a9efdbf9fb029a
TechNing39 <leeyujineee@gmail.com>
2025-04-12 14:27:56 +0900

    [Feat] 회원가입 api 연결

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:ohmo/screen/home_screen.dart';
 import 'package:ohmo/profile_data_provider.dart';
-import 'package:ohmo/screen/login/login_screen.dart';
 import 'package:ohmo/screen/splash_screen.dart';
 
 void main() {
   runApp(
+    MultiProvider(providers: [
     ChangeNotifierProvider(
-      create: (_) => ProfileData(),
+      create: (_) => ProfileData()),
+      ],
       child: MaterialApp(
         theme: ThemeData(
           scaffoldBackgroundColor: Colors.white,
