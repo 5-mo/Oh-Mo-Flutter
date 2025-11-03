5198348ea2f42e5cfcbfaf6ef86f4568580cacb3
TechNing39 <leeyujineee@gmail.com>
2025-11-03 16:09:49 +0900

    [feature] 방 입장하기 첫페이지 초대코드 입력 구현

--- lib/screen/group/group_enter_room_screen.dart
+++ lib/screen/group/group_enter_room_screen.dart
@@ -20,8 +20,10 @@ class GroupEnterRoomScreen extends StatefulWidget {
 class _GroupEnterRoomScreenState extends State<GroupEnterRoomScreen> {
   final db = LocalDatabaseSingleton.instance;
   final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _codeController = TextEditingController();
 
   bool _isPasswordValid = false;
+  bool _isCodeValid = false;
   Timer? _debounce;
 
   late Future<Group?> _groupFuture;
@@ -31,6 +33,7 @@ class _GroupEnterRoomScreenState extends State<GroupEnterRoomScreen> {
     super.initState();
     _groupFuture = db.getGroupById(widget.groupId);
     _passwordController.addListener(_onPasswordChanged);
+    _codeController.addListener(_onCodeChanged);
   }
 
   @override
@@ -38,6 +41,8 @@ class _GroupEnterRoomScreenState extends State<GroupEnterRoomScreen> {
     _debounce?.cancel();
     _passwordController.removeListener(_onPasswordChanged);
     _passwordController.dispose();
+    _codeController.removeListener(_onCodeChanged);
+    _codeController.dispose();
     super.dispose();
   }
 
@@ -54,6 +59,24 @@ class _GroupEnterRoomScreenState extends State<GroupEnterRoomScreen> {
       if (mounted) {
         setState(() {
           _isPasswordValid = exists;
+        });
+      }
+    });
+  }
+
+  void _onCodeChanged() {
+    if (_debounce?.isActive ?? false) _debounce!.cancel();
+    _debounce = Timer(const Duration(milliseconds: 500), () async {
+      final code = _codeController.text.trim();
+      if (code.isEmpty) {
+        if (mounted) setState(() => _isCodeValid = false);
+        return;
+      }
+      final bool exists = (code == "ohmo"); // db 연결 필요
+
+      if (mounted) {
+        setState(() {
+          _isCodeValid = exists;
         });
       }
     });
@@ -83,7 +106,7 @@ class _GroupEnterRoomScreenState extends State<GroupEnterRoomScreen> {
               SizedBox(height: 60.0),
               _buildNewRoomHeader(),
               SizedBox(height: 30.0),
-              _buildRoomName(),
+              _buildInvitingCode(),
               SizedBox(height: 60.0),
               _buildRoomPassword(_passwordController),
               SizedBox(height: 180.0),
@@ -105,7 +128,7 @@ class _GroupEnterRoomScreenState extends State<GroupEnterRoomScreen> {
           ),
           SizedBox(height: 100),
           Text(
-            '입장하려는 방의 이름을 확인하세요',
+            '입장하려는 방의 초대 코드를 붙여 넣으세요',
             style: TextStyle(fontFamily: 'PretendardMedium', fontSize: 13.0),
           ),
         ],
@@ -113,7 +136,7 @@ class _GroupEnterRoomScreenState extends State<GroupEnterRoomScreen> {
     );
   }
 
-  Widget _buildRoomName() {
+  Widget _buildInvitingCode() {
     return Container(
       width: 214,
       height: 37,
@@ -126,10 +149,49 @@ class _GroupEnterRoomScreenState extends State<GroupEnterRoomScreen> {
           BoxShadow(color: Colors.black.withOpacity(0.3), blurRadius: 3),
         ],
       ),
-      child: Text(
-        textAlign: TextAlign.center,
-        '사이드 프로젝트',
-        style: TextStyle(fontFamily: 'PretendardRegular', fontSize: 12.0),
+      child: Row(
+        children: [
+          SizedBox(width: 15),
+          Expanded(
+            child: TextField(
+              controller: _codeController,
+              autofocus: true,
+              style: TextStyle(
+                fontSize: 15,
+                fontFamily: 'PretendardMedium',
+                color: Colors.black,
+              ),
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(bottom: 10, left: 30),
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          IconButton(
+            icon: SvgPicture.asset(
+              'android/assets/images/round_check.svg',
+              height: 24,
+              width: 24,
+              colorFilter: ColorFilter.mode(
+                _isCodeValid ? Colors.black : Color(0xFFAFAFAF),
+                BlendMode.srcIn,
+              ),
+            ),
+            onPressed: () {
+              if (_isCodeValid) {
+                final enteredCode = _codeController.text.trim();
+                print('초대 코드: $enteredCode');
+
+                if (mounted) {
+                  Navigator.pop(context);
+                }
+              } else {
+                print('유효하지 않은 코드 입니다.');
+              }
+            },
+          ),
+        ],
       ),
     );
   }
@@ -167,7 +229,7 @@ class _GroupEnterRoomScreenState extends State<GroupEnterRoomScreen> {
                   ],
                   decoration: InputDecoration(
                     border: InputBorder.none,
-                    contentPadding: EdgeInsets.only(bottom: 10,left: 30),
+                    contentPadding: EdgeInsets.only(bottom: 10, left: 30),
                   ),
                   style: TextStyle(
                     fontSize: 15,
@@ -199,13 +261,17 @@ class _GroupEnterRoomScreenState extends State<GroupEnterRoomScreen> {
     return Center(
       child: GestureDetector(
         onTap: () {
-          if (_isPasswordValid) {
+          if (_isPasswordValid && _isCodeValid) {
             Navigator.push(
               context,
               MaterialPageRoute(
                 builder: (context) => GroupFinalEnterRoomScreen(),
               ),
             );
+          } else if (!_isCodeValid) {
+            ScaffoldMessenger.of(
+              context,
+            ).showSnackBar(SnackBar(content: Text("초대 코드를 확인해주세요")));
           } else {
             ScaffoldMessenger.of(
               context,
